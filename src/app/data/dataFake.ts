4032706a695b49b99b2f69807066449100849bbf
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-05-16 11:55:06 -0500

    Feat(content): Carregar dados dinâmicos com base no ID da rota
    - Substituir a constante 'data' por 'dataFake' e ajustar os campos para corresponder à estrutura dos componentes
    - Utilizar ActivatedRoute para capturar o parâmetro 'id' e buscar os dados correspondentes no array dataFake
    - Popular dinamicamente os campos title, description e photoCover do ContentComponent
    - Remover borda do layout de conteúdo e ajustar espaçamento
    - Atualizar título da página HTML para "Informer BLOG"

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,29 @@
-export const data = [
+export const dataFake = [
   {
-    id: 1,
-    photo: 'assets/imagem-noticia-1.png',
-    title: 'MINHA NOTICIA',
+    id: '1',
+    photoCover: 'assets/image-placeholder.png',
+    title: 'NOVO HOMEM DE FERRO ANUNCIADO',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt non aut vero tempora iste corporis esse eos repellat laudantium, beatae adipisci, nihil iure numquam maxime vitae dolorum, dicta eveniet voluptas?',
+      'Novo Homem de Ferro é anunciado e será interpretado por um ator diferente do que já conhecemos. O novo ator é conhecido por seu papel em um filme de sucesso e promete trazer uma nova perspectiva ao personagem. A Marvel está animada com essa nova adição ao seu universo cinematográfico e os fãs estão ansiosos para ver o que está por vir.',
   },
   {
-    id: 2,
-    photo: 'assets/imagem-noticia-1.png',
-    title: 'NOVA NOTICIA',
+    id: '2',
+    photoCover: 'assets/imagem-noticia-1.png',
+    title: 'NOVA SÉRIO ANUNCIADA PELA NETFLIX PARA 2026',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+  },
+  {
+    id: '3',
+    photoCover: 'assets/imagem-noticia-1.png',
+    title: 'TRAILER DE FILME DA MARVEL É REVELADO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+  },
+  {
+    id: '4',
+    photoCover: 'assets/imagem-noticia-1.png',
+    title: 'SÉRIE DE SUCESSO GANHA NOVA TEMPORADA',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
   },
